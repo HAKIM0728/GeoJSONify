3470c39d4b86aedb1b169454b64319e06ea9024e
HAKIM0728 <128070095+HAKIM0728@users.noreply.github.com>
2024-01-22 05:44:45 +0000

    add map

--- app.py
+++ app.py
@@ -3,20 +3,21 @@ import folium
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    # Create a Folium map
-    m = folium.Map(location=[40, -100], zoom_start=4)
+  # Create a Folium map
+  m = folium.Map(location=[40, -100], zoom_start=4)
 
-    # Add a marker to the map
-    folium.Marker(location=[40, -100], popup='Marker').add_to(m)
+  # Add a marker to the map
+  folium.Marker(location=[40, -100], popup='Marker').add_to(m)
 
-    # Convert the Folium map to HTML
-    map_html = m._repr_html_()
+  # Convert the Folium map to HTML
+  map_html = m._repr_html_()
 
-    # Render the HTML template with the map content
-    return render_template('index.html', map_html=map_html)
+  # Render the HTML template with the map content
+  return render_template('index.html', map_html=map_html)
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=True)
+  app.run(host='0.0.0.0', debug=True)
